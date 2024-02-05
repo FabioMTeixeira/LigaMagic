2e4f589f1fed62b9b0abf175d40a66383abc1ca4
fabio <fabio.matosteixeira@gmail.com>
2024-02-05 13:42:47 -0300

    patch: arrumado o rolamento para os lado e cima e baixo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Main from './pages/Main/index.jsx'
+import ShopCard from './pages/ShopCard/index.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Main />
+    <ShopCard />
   </React.StrictMode>,
 )
