ec8c9635e5840761b6f50dc68d11dcc1f1de32e2
fabio <fabio.matosteixeira@gmail.com>
2024-02-05 18:03:07 -0300

    chore: adicionando conteudo sobre a carta

--- src/components/CardInfo/index.jsx
+++ src/components/CardInfo/index.jsx
@@ -1,15 +1,42 @@
+import LetterC from '../../assets/letterC.svg'
+import SetaBaixo from '../../assets/setaBaixo.svg'
 import './style.css'
 
 function CardInfo() {
 
   return (
-    <div className="title">
-      <header>
-        <h3 className='separated'>Home</h3>
-        <h3 className='separated'>Magic : The Gathering</h3>
-        <h3 className='separated'>Busca</h3>
-        <h3>Bazar - Lista de Desejos</h3>
-      </header>
+    <div>
+      <div>
+        <header>
+          <h3 className='separated'>Home</h3>
+          <h3 className='separated'>Magic : The Gathering</h3>
+          <h3 className='separated'>Busca</h3>
+          <h3>Bazar - Lista de Desejos</h3>
+        </header>
+        <section>
+          <img src={LetterC} alt="Letra C" />
+          <h3>#045</h3>
+          <h3>Dominaria Remastered</h3>
+          <button>
+            DMR
+            <img src={SetaBaixo} alt="Seta para baixo" />
+          </button>
+        </section>
+        <details>
+          <div>
+            <h2>Contramágica</h2>
+            <div>
+              <img src="" alt="" />
+              <button>
+                <img src="" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <h3>CounterSpell</h3>
+        </details>
+
+      </div>
     </div>
   )
 }
